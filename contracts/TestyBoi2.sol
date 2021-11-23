00d3d4b4367c484b339bfac29f5597f6928452ed
Matt Baber <mbaber1142@gmail.com>
2021-11-23 10:25:09 -0500

    3. transferFrom, allowance and approval  function

--- contracts/TestyBoi2.sol
+++ contracts/TestyBoi2.sol
@@ -13,8 +13,15 @@ contract TestyBoi2 {
         uint256 _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
 
-    mapping(address => uint256) public balanceOf;    
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance; 
 
     constructor(uint256 _initialSupply) {
         balanceOf[msg.sender] = _initialSupply;
@@ -32,6 +39,31 @@ contract TestyBoi2 {
 
         return true;
     }
+
+    function approve(address _spender, uint256 _value) public returns(bool success) {
+        allowance[msg.sender][_spender] = _value;        
+
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+
+        return true;
+    }
+
+    
+
+    
     
 }
 
